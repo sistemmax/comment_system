cd0f244859a5135593d52fbdac1ef82c6167cd61
Sergey Maximov <sergey@dashtickets.com>
2015-02-19 09:36:50 +1300

    Comment System

---
+++ index.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Comments System</title>
+
+        <link type="text/css" rel="stylesheet" href="css/style.css">
+        <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+    </head>
+    <body>
+
+    <br><br><br><br><br>
+
+
+<?php
+// Connect to the database
+include('config.php');
+$id_post = "1"; //the post or the page id
+?>
+<div class="cmt-container" >
+    <?php
+    $sql = mysql_query("SELECT * FROM comments WHERE id_post = '$id_post'") or die(mysql_error());;
+    while($affcom = mysql_fetch_assoc($sql)){
+        $name = $affcom['name'];
+        $email = $affcom['email'];
+        $comment = $affcom['comment'];
+        $date = $affcom['date'];
+
+        // Get gravatar Image
+        // https://fr.gravatar.com/site/implement/images/php/
+        $default = "mm";
+        $size = 35;
+        $grav_url = "http://www.gravatar.com/avatar/".md5(strtolower(trim($email)))."?d=".$default."&s=".$size;
+
+    ?>
+    <div class="cmt-cnt">
+        <img src="<?php echo $grav_url; ?>" />
+        <div class="thecom">
+            <h5><?php echo $name; ?></h5><span data-utime="1371248446" class="com-dt"><?php echo $date; ?></span>
+            <br>
+            <p>
+                <?php echo $comment; ?>
+            </p>
+        </div>
+    </div><!-- end "cmt-cnt" -->
+    <?php } ?>
+
+
+    <div class="new-com-bt">
+        <span>Write a comment ...</span>
+    </div>
+    <div class="new-com-cnt">
+        <input type="text" id="name-com" name="name-com" value="" placeholder="Your name"  />
+        <span id='name_message'></span>
+        <input type="text" id="mail-com" name="mail-com" value="" placeholder="Your e-mail adress"  />
+        <span id='email_message' ></span>
+        <textarea id='com-com' class="the-new-com" ></textarea>
+        <div class="bt-add-com">Post comment</div>
+        <div class="bt-cancel-com">Cancel</div>
+    </div>
+    <div class="clear"></div>
+</div><!-- end of comments container "cmt-container" -->
+
+
+<script type="text/javascript">
+
+    function validateEmail(email) {
+        var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(email);
+    }
+
+   $(function(){
+        //alert(event.timeStamp);
+        $('.new-com-bt').click(function(event){
+            $(this).hide();
+            $('.new-com-cnt').show();
+            $('#name-com').focus();
+        });
+
+        /* when start writing the comment activate the "add" button */
+        $('.the-new-com').bind('input propertychange', function() {
+           $(".bt-add-com").css({opacity:0.6});
+           var checklength = $(this).val().length;
+           if(checklength){ $(".bt-add-com").css({opacity:1}); }
+        });
+
+        /* on clic  on the cancel button */
+        $('.bt-cancel-com').click(function(){
+            $('.the-new-com').val('');
+            $('.new-com-cnt').fadeOut('fast', function(){
+                $('.new-com-bt').fadeIn('fast');
+            });
+        });
+
+        // on post comment click
+        $('.bt-add-com').click(function(){
+            var theCom = $('.the-new-com');
+            var theName = $('#name-com');
+            var theMail = $('#mail-com');
+
+            if( !theCom.val()){
+                alert('You need to write a comment!');
+            }
+            if(!validateEmail(theMail.val())) {
+                $('#email_message').html(function() {
+                  return "Please check your Email";
+                });
+            }
+
+            else{
+                $.ajax({
+                    type: "POST",
+                    url: "ajax/add-comment.php",
+                    data: 'act=add-com&id_post='+<?php echo $id_post; ?>+'&name='+theName.val()+'&email='+theMail.val()+'&comment='+theCom.val(),
+                    success: function(html){
+                        theCom.val('');
+                        theMail.val('');
+                        theName.val('');
+                        $('.new-com-cnt').hide('fast', function(){
+                            $('.new-com-bt').show('fast');
+                            $('.new-com-bt').before(html);
+                        })
+                    }
+                });
+            }
+        });
+
+    });
+</script>
+
+</body>
+</html>
